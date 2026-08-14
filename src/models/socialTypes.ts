cb283f9e23942981efa651ed73c60b3e73523de7
Ewawere <153601310+Ewawere@users.noreply.github.com>
2026-08-14 13:58:40 +0100

    Add Phase 11 Social and Media types

---
+++ src/models/socialTypes.ts
@@ -0,0 +1,47 @@
+import { Player, Club } from './types';
+
+export enum SocialActorType {
+  FAN = 'FAN',
+  CLUB = 'CLUB',
+  JOURNALIST = 'JOURNALIST',
+  FORMER_PLAYER = 'FORMER_PLAYER',
+  TEAMMATE = 'TEAMMATE',
+  RIVAL_FAN = 'RIVAL_FAN'
+}
+
+export interface SocialPost {
+  id: string;
+  actorName: string;
+  actorType: SocialActorType;
+  content: string;
+  likes: number;
+  hashtags: string[];
+  timestamp: Date;
+}
+
+export interface InterviewQuestion {
+  id: string;
+  text: string;
+  options: InterviewOption[];
+}
+
+export interface InterviewOption {
+  text: string;
+  personalityImpact: Partial<PersonalityImpact>;
+  relationshipImpact: Partial<RelationshipImpact>;
+}
+
+export interface PersonalityImpact {
+  teamPlayer: number;
+  confidence: number;
+  ambition: number;
+  loyalty: number;
+  ego: number;
+  professionalism: number;
+}
+
+export interface RelationshipImpact {
+  managerTrust: number;
+  fanApproval: number;
+  teammateUnity: number;
+}
